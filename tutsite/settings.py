a4ab21b9f96590342bdcaab4312dd0cab71add21
dereknowicki <dereknowicki@gmail.com>
2015-10-04 07:14:20 +0000

    change settings to use mysql database

--- tutsite/settings.py
+++ tutsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.8/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+import MySQLdb
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -78,8 +79,11 @@ WSGI_APPLICATION = 'tutsite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'dereknowicki$tutorial_site',
+        'USER': 'dereknowicki',
+        'PASSWORD': 'dsn9851*',
+        'HOST': 'dereknowicki.mysql.pythonanywhere-services.com',
     }
 }
 
